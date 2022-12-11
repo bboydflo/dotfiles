34c68f8110bbd8196fcab40123c947f3f2b3907a
Florin Onciu <dkFloOnc@MBP-M1-Fo.local>
2022-12-12 00:23:07 +0100

    prepare to run within real macos system

--- run_once_install-1-brew.sh
+++ run_once_install-1-brew.sh
@@ -7,8 +7,6 @@ function installHomebrew() {
     echo "==================================="
     echo "                                   "
 
-    cd ~/
-
     which -s brew > /dev/null
     if [[ $? -eq 1 ]]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -25,23 +23,6 @@ function installHomebrewPackages() {
     brew bundle install
 
     brew update
-}
-
-function installAptPackages() {
-
-    echo "==================================="
-    echo "apt update - update list of available packages"
-    echo "==================================="
-    echo "                                   "
-    apt update
-
-    echo "==================================="
-    echo "apt install - install packages"
-    echo "==================================="
-    echo "                                   "
-    apt install tmux fzf # fish
-    # install fisher
-    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 }
 
 installHomebrew
